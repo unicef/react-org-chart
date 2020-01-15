58c78563e07b5acae450174b934763a51d3fcff1
sureshsevarthi <sevarthi.suresh@gmail.com>
2020-01-15 14:16:28 -0500

    export org chart image as svg

--- src/chart/exportOrgChartImage.js
+++ src/chart/exportOrgChartImage.js
@@ -1,5 +1,3 @@
-const d3 = require('d3')
-
 module.exports = exportOrgChartImage
 
 function exportOrgChartImage({ loadConfig }) {
@@ -7,67 +5,52 @@ function exportOrgChartImage({ loadConfig }) {
   const { id, downlowdedOrgChart, nodeLeftX, nodeRightX, nodeY } = config
   var w = nodeLeftX + nodeRightX
   var h = nodeY
-  var ratio = 2
-
-  // checking wether it has canvas in the convas-container div
-  document.getElementById(`${id}-canvas-container`).querySelector('canvas')
-    ? document
-      .getElementById(`${id}-canvas-container`)
-      .querySelector('canvas')
-      .remove()
-    : ''
-
-  // creating a canvas element
-  var canvas1 = document.createElement('canvas')
-  canvas1.id = 'canvas1'
-  canvas1.width = w * ratio
-  canvas1.height = h * ratio
-  document.getElementById(`${id}-canvas-container`).appendChild(canvas1)
 
   // creating duplicate org chart svg from original org chart svg
-  var step = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
-  step.id = 'newsvg'
-  step.setAttribute('width', w)
-  step.setAttribute('height', h)
-  step.setAttribute('viewBox', `${-nodeLeftX} 0 ${w} ${h + 200}`)
-  step.innerHTML = document.getElementById('svg').innerHTML
+  var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+  svg.id = 'newsvg'
+  svg.setAttribute('width', w)
+  svg.setAttribute('height', h)
+  svg.setAttribute('viewBox', `${-nodeLeftX} 0 ${w} ${h + 200}`)
+  svg.innerHTML = document.getElementById('svg').innerHTML
 
   document.getElementById(`${id}-svg-container`).querySelector('svg')
     ? document
-      .getElementById(`${id}-svg-container`)
-      .querySelector('svg')
-      .remove()
+        .getElementById('#react-org-chart-svg-container')
+        .querySelector('svg')
+        .remove()
     : ''
-  document.getElementById(`${id}-svg-container`).appendChild(step)
+  document.getElementById(`${id}-svg-container`).appendChild(svg)
 
   // appending g element from svg
-  const g = document
-    .getElementById(`${id}-svg-container`)
-    .querySelector('g')
+  var g = document.getElementById(`${id}-svg-container`).querySelector('g')
   g.setAttribute('transform', `translate(0,0)`)
-  var html = new XMLSerializer().serializeToString(
-    document
-      .getElementById(`${id}-svg-container`)
-      .querySelector('svg')
-  )
 
   // generating image with base 64
-  var imgSrc = 'data:image/svg+xml;base64,' + btoa(html)
-  let canvas = document.getElementById('canvas1')
-  let context = canvas.getContext('2d')
-  let image = new Image()
-  image.src = imgSrc
+  var serializer = new XMLSerializer()
+  var source = serializer.serializeToString(svg)
 
-  // downloading the image
-  image.onload = function () {
-    context.drawImage(image, 0, 0, canvas.width, canvas.height)
-    canvas.toBlob(function (blob) {
-      let a = document.createElement('a')
-      let url = URL.createObjectURL(blob)
-      a.download = 'orgchart.jpg'
-      a.href = url
-      a.click()
-    })
-    downlowdedOrgChart(true)
+  //add name spaces.
+  if (!source.match(/^<svg[^>]+xmlns="http\:\/\/www\.w3\.org\/2000\/svg"/)) {
+    source = source.replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"')
   }
+  if (!source.match(/^<svg[^>]+"http\:\/\/www\.w3\.org\/1999\/xlink"/)) {
+    source = source.replace(
+      /^<svg/,
+      '<svg xmlns:xlink="http://www.w3.org/1999/xlink"'
+    )
+  }
+
+  //add xml declaration
+  source = '<?xml version="1.0" standalone="no"?>\r\n' + source
+
+  //convert svg source to URI data scheme.
+  var url = 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(source)
+
+  var a = document.createElement('a')
+  a.download = 'orgchart.svg'
+  a.href = url
+  a.click()
+
+  downlowdedOrgChart(true)
 }
