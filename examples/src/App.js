f41cca4d3dc07b930161bcab801473469a04a5a5
sureshsevarthi <sevarthi.suresh@gmail.com>
2020-02-04 16:39:48 -0500

    Updated the example app to use the org-chart

--- examples/src/App.js
+++ examples/src/App.js
@@ -1,19 +1,7 @@
 import React from 'react'
 import './App.css'
 import OrgChart from '@unicef/react-org-chart'
-
-function getChild(id) {
-  switch (id) {
-    case 100:
-      return tree1
-    case 36:
-      return tree2
-    case 56:
-      return tree3
-    default:
-      return console.log('no children')
-  }
-}
+import { HashRouter, Route, Link } from 'react-router-dom'
 
 const tree = {
   id: 100,
@@ -21,9 +9,9 @@ const tree = {
     id: 100,
     avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/spbroma/128.jpg',
     department: '',
-    name: 'Henry',
+    name: 'Henry monger',
     title: 'Manager',
-    totalReports: 1,
+    totalReports: 3,
   },
   hasChild: true,
   hasParent: true,
@@ -37,13 +25,41 @@ const tree1 = [
       id: 36,
       avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/spbroma/128.jpg',
       department: '',
-      name: 'Tomasz',
+      name: 'Tomasz polaski',
       title: 'IT Specialist',
-      totalReports: 1,
+      totalReports: 4,
     },
     hasChild: true,
     hasParent: true,
     // children: [],
+  },
+  {
+    id: 32,
+    person: {
+      id: 32,
+      avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/spbroma/128.jpg',
+      department: '',
+      name: 'Emanuel walker',
+      title: 'IT Specialist',
+      totalReports: 0,
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [],
+  },
+  {
+    id: 25,
+    person: {
+      id: 25,
+      avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/spbroma/128.jpg',
+      department: '',
+      name: 'Kerry peter',
+      title: 'IT Specialist',
+      totalReports: 0,
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [],
   },
 ]
 
@@ -56,12 +72,42 @@ const tree2 = [
       department: '',
       name: 'Sam John',
       title: 'HR',
-      totalReports: 1,
+      totalReports: 2,
       link: 'https://github.com/unicef/react-org-chart',
     },
     hasChild: true,
     hasParent: true,
     // children: [],
+  },
+  {
+    id: 66,
+    person: {
+      id: 66,
+      avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/spbroma/128.jpg',
+      department: '',
+      name: 'John doe',
+      title: 'Developer',
+      totalReports: 0,
+      link: 'https://github.com/unicef/react-org-chart',
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [],
+  },
+  {
+    id: 76,
+    person: {
+      id: 76,
+      avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/spbroma/128.jpg',
+      department: '',
+      name: 'Emilia rogers',
+      title: 'Developer',
+      totalReports: 0,
+      link: 'https://github.com/unicef/react-org-chart',
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [],
   },
   {
     id: 60,
@@ -70,7 +116,7 @@ const tree2 = [
       avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/spbroma/128.jpg',
       department: '',
       name: 'Ellen cott',
-      title: 'CEO',
+      title: 'IT Officer',
       totalReports: 0,
     },
     hasChild: false,
@@ -87,7 +133,21 @@ const tree3 = [
       avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/spbroma/128.jpg',
       department: '',
       name: 'Kenneth dom',
-      title: 'CEO',
+      title: 'IT Officer',
+      totalReports: 0,
+    },
+    hasChild: false,
+    hasParent: true,
+    children: [],
+  },
+  {
+    id: 45,
+    person: {
+      id: 45,
+      avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/spbroma/128.jpg',
+      department: '',
+      name: 'Kin baker',
+      title: 'IT Officer',
       totalReports: 0,
     },
     hasChild: false,
@@ -96,64 +156,158 @@ const tree3 = [
   },
 ]
 
-export default function App() {
-  const treeData = tree
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
 
-  return (
-    <React.Fragment>
-      <button id="zoom-in">+</button>
-      <button id="zoom-out">-</button>
-      <button id="download-image">Download as image</button>
-      <button id="download-pdf">Download as PDF</button>
-      <OrgChart
-        tree={treeData}
-        loadImage={d => {
-          return Promise.resolve(
-            'https://s3.amazonaws.com/uifaces/faces/twitter/spbroma/128.jpg'
-          )
-        }}
-        loadParent={d => {
-          if (d.id === 100) {
-            return {
-              id: 500,
-              person: {
-                id: 500,
-                avatar:
-                  'https://s3.amazonaws.com/uifaces/faces/twitter/spbroma/128.jpg',
-                department: '',
-                name: 'Pascal ruth',
-                title: 'Member',
-                totalReports: 1,
-              },
-              hasChild: false,
-              hasParent: true,
-              children: [d],
-            }
-          } else if (d.id === 500) {
-            return {
-              id: 1,
-              person: {
-                id: 1,
-                avatar:
-                  'https://s3.amazonaws.com/uifaces/faces/twitter/spbroma/128.jpg',
-                department: '',
-                name: 'Bryce joe',
-                title: 'Director',
-                totalReports: 1,
-              },
-              hasChild: false,
-              hasParent: false,
-              children: [d],
-            }
-          } else {
-            return d
-          }
-        }}
-        loadChildren={d => {
-          const childrenData = getChild(d.id)
-          return childrenData
-        }}
-      />
-    </React.Fragment>
-  )
+    this.state = {
+      tree: tree,
+      downloadingChart: false,
+      config: {},
+      highlightPostNumbers: [1],
+    }
+  }
+
+  getChild = id => {
+    switch (id) {
+      case 100:
+        return tree1
+      case 36:
+        return tree2
+      case 56:
+        return tree3
+      default:
+        return console.log('no children')
+    }
+  }
+
+  getParent = d => {
+    if (d.id === 100) {
+      return {
+        id: 500,
+        person: {
+          id: 500,
+          avatar:
+            'https://s3.amazonaws.com/uifaces/faces/twitter/spbroma/128.jpg',
+          department: '',
+          name: 'Pascal ruth',
+          title: 'Member',
+          totalReports: 1,
+        },
+        hasChild: false,
+        hasParent: true,
+        children: [d],
+      }
+    } else if (d.id === 500) {
+      return {
+        id: 1,
+        person: {
+          id: 1,
+          avatar:
+            'https://s3.amazonaws.com/uifaces/faces/twitter/spbroma/128.jpg',
+          department: '',
+          name: 'Bryce joe',
+          title: 'Director',
+          totalReports: 1,
+        },
+        hasChild: false,
+        hasParent: false,
+        children: [d],
+      }
+    } else {
+      return d
+    }
+  }
+
+  handleDownload = () => {
+    this.setState({ downloadingChart: false })
+  }
+
+  handleOnChangeConfig = config => {
+    this.setState({ config: config })
+  }
+
+  handleLoadConfig = () => {
+    const { config } = this.state
+    return config
+  }
+
+  render() {
+    const { tree, downloadingChart } = this.state
+
+    //For downloading org chart as image or pdf based on id
+    const downloadImageId = 'download-image'
+    const downloadPdfId = 'download-pdf'
+
+    return (
+      <HashRouter basename="/">
+        <Route exact path="/">
+          <React.Fragment>
+            <div className="buttons-container">
+              <div className="zoom-buttons">
+                <button
+                  className="btn btn-outline-primary zoom-button"
+                  id="zoom-in"
+                >
+                  +
+                </button>
+                <button
+                  className="btn btn-outline-primary zoom-button"
+                  id="zoom-out"
+                >
+                  -
+                </button>
+              </div>
+              <div className="">
+                <button className="btn btn-outline-primary" id="download-image">
+                  Download as image
+                </button>
+                <button className="btn btn-outline-primary" id="download-pdf">
+                  Download as PDF
+                </button>
+                <a
+                  className="github-link"
+                  href="https://github.com/unicef/react-org-chart"
+                >
+                  Github
+                </a>
+                {downloadingChart && <div>Downloading chart</div>}
+              </div>
+            </div>
+            <OrgChart
+              tree={tree}
+              downloadImageId={downloadImageId}
+              downloadPdfId={downloadPdfId}
+              onConfigChange={config => {
+                this.handleOnChangeConfig(config)
+              }}
+              loadConfig={d => {
+                let configuration = this.handleLoadConfig(d)
+                if (configuration) {
+                  return configuration
+                }
+              }}
+              downlowdedOrgChart={d => {
+                this.handleDownload()
+              }}
+              loadImage={d => {
+                return Promise.resolve(
+                  'https://s3.amazonaws.com/uifaces/faces/twitter/spbroma/128.jpg'
+                )
+              }}
+              loadParent={d => {
+                console.log(d)
+                const parentData = this.getParent(d)
+                return parentData
+              }}
+              loadChildren={d => {
+                const childrenData = this.getChild(d.id)
+                return childrenData
+              }}
+            />
+          </React.Fragment>
+        </Route>
+      </HashRouter>
+    )
+  }
 }
